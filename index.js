cdacbde042b10d04f0a9db8fe532bdd8cac25976
Jaishanu <jaishanu2410@gmail.com>
2025-07-28 14:37:23 +0530

    back4app

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.static(__dirname+'/public'));
 
 //send home page
 app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname+'/public/index.html');
 });
 //send set password page
 app.get('/setpassword/:id',function(req,res){
